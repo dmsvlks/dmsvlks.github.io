d0215536ec2a8b0f14db67fecf5b255b99219114
Domas <domas.velykis@gmail.com>
2020-06-18 10:22:01 +0300

    Updates

---
+++ precache-manifest.8621d8f6a5b8ad9796e5ce927c2018d3.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "485e018b8601c9f29263",
+    "url": "/static/js/app.73d14b18.chunk.js"
+  },
+  {
+    "revision": "3636aebd34579cc662c5",
+    "url": "/static/js/runtime~app.4b0f5a04.js"
+  },
+  {
+    "revision": "bc30e9273449407a62b3",
+    "url": "/static/js/2.6dc17564.chunk.js"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "a85be8301e6f25bca8dbcb2455d37460",
+    "url": "/static/media/artimiausias_icon.a85be830.png"
+  },
+  {
+    "revision": "a591306cac7dd4a9109e96e1973a1ee4",
+    "url": "/static/media/card-visa.a591306c.png"
+  },
+  {
+    "revision": "9351e219a0de7047dc36e12bb326a526",
+    "url": "/static/media/card-mastercard.9351e219.png"
+  },
+  {
+    "revision": "b267e73572d2fb8f192727e0e73eee2d",
+    "url": "/static/media/card-unknown.b267e735.png"
+  },
+  {
+    "revision": "782a8692856c4a67f010e577008fec61",
+    "url": "/static/media/visa.782a8692.png"
+  },
+  {
+    "revision": "cb38652a02a27ed79fb7f1bc6cbcbc64",
+    "url": "/static/media/mastercard.cb38652a.png"
+  },
+  {
+    "revision": "5387abe0995fc9c03aee49142321460c",
+    "url": "/static/media/unknown.5387abe0.png"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "bc203d8c5ff1fe30ea56c762adee52b9",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "d4e6917973e4e4675f4fe0128bfa5417",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "44318850a72f084309905047539010ce",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "959bbb2f9598121b79cb2bd185a367ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fea80e5263b447392d0e3168eda2760a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "4197a9fd61e635cea7da7906870ef364",
+    "url": "/register-service-worker.js"
+  }
+];
